791662ac780ee025581fa1f80a63430511eed1e8
Uma Mahesh Pagidoju <pagidojuumamahesh@gmail.com>
2025-06-20 10:26:11 +0530

    final changes to make deployment work

--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -2,9 +2,9 @@ const NavBar = ({ setSearchTerm }) => {
     return(
         <>
             <div className="navbar">
-                <a href="/" className="icon-link"><div className="icon" ><h1>Flick Finder</h1></div></a>
+                <a href="/MovieList/" className="icon-link"><div className="icon" ><h1>Flick Finder</h1></div></a>
                 <div className="navs">
-                    <a href="/">Home</a>
+                    <a href="/MovieList/">Home</a>
                     <button className="navbtns" onClick={() => setSearchTerm("series")}>TV Shows</button>
                     <button className="navbtns" onClick={() => setSearchTerm("movie")}>Movies</button>
                     <button className="navbtns">Upcoming Movies</button>
